b831934922a91265cfcfea2ddf905fb2bd4705f1
leoale95 <131513332+leoale95@users.noreply.github.com>
2023-11-20 16:28:30 +1100

    API - MVC - Persistencia

--- src/Daos/dto/user.dto.js
+++
@@ -1,8 +0,0 @@
-export class UserGetDto {
-	constructor(userDB) {
-		(this.first_name = userDB.first_name),
-			(this.last_name = userDB.last_name),
-			(this.email = userDB.email),
-			(this.role = userDB.role);
-	}
-}
